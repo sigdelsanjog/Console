5ce8ca8772ef10e7bfcbcf9bbe196a87b88176b0
sigdelsanjog <sigdelsanjog@gmail.com>
2021-03-16 03:34:37 -0700

    minor change

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ namespace Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-	    Console.WriteLine("Bamboo Version Update CHeck");
         }
     }
 }
